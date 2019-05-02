8df68e4dc5e934e206c033485c1d165d307f9d8a
Derek Zhang <chromestone@users.noreply.github.com>
2019-05-02 15:49:10 -0400

    started train
    just a little

--- train.py
+++ train.py
@@ -1,2 +1,15 @@
-print(len(sys.argv)
+print(len(sys.argv))
 print("hello world")
+
+import keras.utils as ku
+import matplotlib.pyplot as plt
+
+from data import BeeLoader
+from model import build_model
+
+bee_data = BeeLoader()
+bee_model = build_model()
+
+for images, ids in bee_data.generator(6):
+  pass
+  #bee_model.train([images], [id])
