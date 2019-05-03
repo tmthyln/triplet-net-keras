e77beb00664ace7fd284248e5fbe4a91880c2caa
Derek Zhang <chromestone@users.noreply.github.com>
2019-05-03 08:53:31 -0400

    Update loss.py

--- loss.py
+++ loss.py
@@ -1,4 +1,8 @@
 import tensorflow as tf
 
 def custom_loss(y_true, y_pred):
-  pass
+
+  foo = tf.constant(.75, dtype=tf.float32)
+  goo = tf.subtract(y_true, y_pred)
+  shoe = tf.reduce_mean(goo)
+  return tf.multiply(foo, shoe)
